2be8b450d0a23597fb242f252609bcef369e670c
Jonathan Kim <jonkim204@gmail.com>
2018-06-18 11:41:14 -0700

    nameSpace part 1

--- scripts/index.js
+++ scripts/index.js
@@ -3,17 +3,6 @@
 // eslint-disable-next-line no-unused-vars
 'use strict'
 
-const store = {
-  items: [
-    { id: cuid(), name: 'apples', checked: false },
-    { id: cuid(), name: 'oranges', checked: false },
-    { id: cuid(), name: 'milk', checked: true },
-    { id: cuid(), name: 'bread', checked: false }
-  ],
-  hideCheckedItems: false,
-  searchTerm: ''
-};
-
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
