47ff6c3cb1aadc94e3ac187c82a3458145663df9
Akim Desire <adesi006.ng@gmail.com>
2018-06-18 15:08:49 -0400

    finished UpdateSHoppinglist drill 2

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -59,7 +59,14 @@ const shoppingList = (function(){
   
   
   function addItemToShoppingList(itemName) {
-    store.items.push({ id: cuid(), name: itemName, checked: false });
+    try{
+      Item.validateName(itemName);
+      store.items.push(Item.create(itemName));
+      render();
+    }
+    catch(error){
+      console.log('Cannot add item: {error.message}')
+    }
   }
   
   function handleNewItemSubmit() {
