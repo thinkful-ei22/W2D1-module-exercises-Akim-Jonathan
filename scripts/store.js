3a2ce3e41d5a538d00b6f0ca947149d11171bd71
Jonathan Kim <jonkim204@gmail.com>
2018-06-18 13:57:42 -0700

    VERY LONG DRILL

--- scripts/store.js
+++ scripts/store.js
@@ -9,8 +9,42 @@ const store = (function () {
   ];
   const hideCheckedItems = false;
   const searchTerm = '';
+  const findById = function(id) {
+    return store.items.find(items =>items.id === id );
+  };
+  const addItem = function(name) {
+    try {
+      Item.validateName(name);
+      this.items.push(Item.create(name));
+    }
+    catch {
+      console.log( 'Cannot add item: {error.message}');
+    }
+  }
+  const findAndToggleChecked = function(id) {
+      if (this.findById(id).checked === true) {
+         this.findById(id).checked = false;
+      }
+      if (this.findById(id).checked === false) {
+         this.findById(id).checked = true;
+      }
+  };
+  const findAndUpdateName = function(id,newName) {
+    try {
+      Item.validateName(newName);
+  }
+    catch {
+      console.log( 'Cannot add item: {error.message}');
+  }
+    findById(id).name = newName;
+  };
 
+ const findAndDelete = function(id) {
+    // this.items.splice([this.items.findIndex(findById(id))], 1)
+    const filtered = this.items.filter(item => item.id !== id);
+    this.items = filtered;
+ };
   return {
-    items, hideCheckedItems, searchTerm
+    items, hideCheckedItems, searchTerm, findById, addItem, findAndToggleChecked, findAndUpdateName, findAndDelete
   };
 }());
